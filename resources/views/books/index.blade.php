3ea0893ef8ff021d16c530dbfa27c557c1d33ad6
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-10-24 09:50:57 +0200

    publisher migrations

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -11,7 +11,9 @@
             <img src="{{ $book->image }}" alt="title"/><br><br>
             <h2>{{ $book->title }}</h2>
             <h3>Author: {{ $book->authors }}</h3>
+            @if(isset($book->genres_id))
             <h3>Genre: {{ $genres[$book->genres_id-1]->name }}</h3>
+            @endif
             <a href="{{action('BookController@edit', [$book->id])}}" class="btn btn-success">Update</a>
             <a href="{{action('BookController@delete', [$book->id])}}" class="btn btn-danger">Delete</a>
             <a href="{{action('BookController@show', [$book->id])}}" class="btn btn-success">Display</a>
